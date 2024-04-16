914b93d7d57d907b47670c21bc1c384d2804c26f
ChristianPDAG <christian.perezdearce@gmail.com>
2024-04-16 18:02:42 -0400

    avance imc calculator + menu

---
+++ app/src/main/java/com/chris/myfirstapp/MenuActivity.kt
@@ -0,0 +1,27 @@
+package com.chris.myfirstapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
+import com.chris.myfirstapp.imccalculator.IMCAppActivity
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+        val btnSaludApp = findViewById<AppCompatButton>(R.id.btnSaludApp)
+        val btnIMCApp = findViewById<AppCompatButton>(R.id.btnIMCApp)
+        btnSaludApp.setOnClickListener{ navigateToSaludApp()  }
+        btnSaludApp.setOnClickListener{ navigateToIMCApp()  }
+
+    }
+    private fun navigateToIMCApp(){
+        val intent = Intent(this, IMCAppActivity::class.java)
+        startActivity(intent)
+    }
+    private fun navigateToSaludApp(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/chris/myfirstapp/imccalculator/IMCAppActivity.kt
@@ -0,0 +1,12 @@
+package com.chris.myfirstapp.imccalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.chris.myfirstapp.R
+
+class IMCAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imcapp)
+    }
+}
